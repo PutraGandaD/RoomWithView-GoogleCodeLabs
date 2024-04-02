1b5139e2a3d7a32a9d857a162a81cdfdf0cc6f70
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-04-02 22:26:45 +0700

    Finished Word Dao

---
+++ app/src/main/java/com/putragandad/roomnotesmvvm/WordDao.kt
@@ -0,0 +1,24 @@
+package com.putragandad.roomnotesmvvm
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+// Dao = Data Access Object
+@Dao
+interface WordDao {
+    // Getting all words ordered alphabetically
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): Flow<List<Word>> // Use Flow to produces values one at a time
+
+    // Inserting a word
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    // Deleting all words
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
+
