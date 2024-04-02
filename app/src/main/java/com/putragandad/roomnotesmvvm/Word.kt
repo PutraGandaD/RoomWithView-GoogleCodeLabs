c207b4a77c04c3caa749fcd1b9d3cb900b1759b6
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-04-02 22:17:20 +0700

    Finished Word Data Class / Entity

--- app/src/main/java/com/putragandad/roomnotesmvvm/Word.kt
+++ app/src/main/java/com/putragandad/roomnotesmvvm/Word.kt
@@ -1,3 +1,10 @@
 package com.putragandad.roomnotesmvvm
 
-data class Word(val word: String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "word") val word: String)
